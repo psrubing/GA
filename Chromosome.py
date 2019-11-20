628a555600d09a067d9214195170e4abcfe989b4
Piotr Sawicki <37271385+psrubing@users.noreply.github.com>
2019-11-20 13:12:56 +0100

    Changes for simulation (water layer radius added)

--- Chromosome.py
+++ Chromosome.py
@@ -1,35 +1,39 @@
 import random
+import math
 
 class Chromosome:
 
-    chromosome_length = 16
+    chromosome_length = 32
 
     def __init__(self,chromosome_list=None):
 
         if chromosome_list is None:
-            self.chromosome_dec = random.randint(0, (2 ** 16) - 1)
+            self.chromosome_dec = random.randint(0, (2 ** self.chromosome_length) - 1)
             self.chromosome_bin = bin(self.chromosome_dec)[2:].zfill(self.chromosome_length)
             self.chromosome_list = [int(i) for i in self.chromosome_bin]
         else:
             self.chromosome_list = chromosome_list
             self.chromosome_dec = int("".join(str(x) for x in self.chromosome_list), 2)
             self.chromosome_bin = bin(self.chromosome_dec)[2:].zfill(self.chromosome_length)
-        self.keff = 0.0
+        self.keff = 0
 
+    def get_uranium_radius(self):
+        value =  int(self.chromosome_bin[0:16],base=2)
+        return 1 + (value * 14.0) / float((2 ** 16) - 1)
 
-    def get_variable(self):
-        return (self.chromosome_dec*15.75) / float((2 ** 16) - 1)
-        # zamienia chromosom na liczbe z przedziału (0,15.75)
+    def get_water_radius(self):
+        value = int(self.chromosome_bin[16:32],base=2)
+        return 15 + (value * 35) / float((2 ** 16) - 1)
 
     def get_fitness(self):
-        sum = 0
-        for gene in self.chromosome_list:
-            sum += gene
-        return sum
+        dens_uranium = 10.1
+        dens_water = 1.0
+        vol_uranium = 4.2*(self.get_uranium_radius())**3
+        vol_water = 4.2*(self.get_water_radius()-self.get_uranium_radius())**3
+        return 1 / (abs(1 - self.keff) * math.sqrt(dens_uranium * vol_uranium + dens_water * vol_water))
 
     def __str__(self):
-        return "Chromosome with {} fitness, variable: {}, list: {}, dec: {}, bin: {}".format(self.get_fitness(), self.get_variable(), self.chromosome_list,
-                                                                                             self.chromosome_dec, self.chromosome_bin)
-
+        return "Chromosome with {} fitness, uranium radius: {}, water radius: {}, keff: {}, list: {}, dec: {}, bin: {}".format(self.get_fitness(), self.get_uranium_radius(), self.get_water_radius(),
+                                                                                                                               self.keff, self.chromosome_list, self.chromosome_dec, self.chromosome_bin)
 
 
