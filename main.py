64d5508749427f82a137af28abea49c7dd255c2b
Piotr Sawicki <37271385+psrubing@users.noreply.github.com>
2019-11-20 13:13:41 +0100

    Update main

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 from Population import Population
 
-def main():
 
-    num_generations = 100
+def main():
+    num_generations = 50
     pop_size = 50
+    mutation_prob = 0.01
+    best_path = "/home/piotrs/GA/Best_Chromosomes.txt"
 
     pop = Population(pop_size)
     pop.make_population()
-    print("First pop fitness: ", pop.calc_pop_fitness())
+    print("Zero's pop fitness: ", pop.calc_pop_fitness())
+
+    best_input = open(best_path, "a+")
+    best_input.write("Generation\tBest Fitness\t\tChromosome:\n")
 
     for generation in range(num_generations):
 
         new_pop = Population(pop_size)
 
-        for i in range(int(pop_size/2)):
-
+        for i in range(int(pop_size / 2)):
             parent1 = pop.roulette()
             parent2 = pop.roulette()
+
             print("parent1: ", parent1)
             print("parent2: ", parent2)
 
             child1, child2 = pop.crossover(parent1, parent2)
+
             print("child1: ", child1)
             print("child2: ", child2)
 
@@ -28,11 +34,16 @@ def main():
             new_pop.chromosomes.append(child2)
 
         pop = new_pop
-        pop.mutation(0.01)
-        pop.generation += 1
+        pop.generation = generation + 1
+        pop.mutation(mutation_prob)
+        pop.makedir()
+        pop.simulation()
 
-        print("Best chromosome from pop: ",pop.best_chromosome())
-        print(f"New pop nr. {generation} fitness: ", pop.calc_pop_fitness())
+        print("Best chromosome from population nr. {} : {}\n".format(pop.generation, pop.best_chromosome()))
+        print(f"New pop nr. {pop.generation} fitness: ", pop.calc_pop_fitness())
+        best_input.write(str(pop.generation)+"\t\t"+str(pop.best_chromosome(1))+"\t"+str(pop.best_chromosome(0))+"\n")
+
+    best_input.close()
 
 
 if __name__ == '__main__':
