888e07de725a19a9dec52e7946c8ccd639497c46
Piotr Sawicki <37271385+psrubing@users.noreply.github.com>
2019-11-26 13:45:32 +0100

    Update Chromosome.py

--- Chromosome.py
+++ Chromosome.py
@@ -15,22 +15,22 @@ class Chromosome:
             self.chromosome_list = chromosome_list
             self.chromosome_dec = int("".join(str(x) for x in self.chromosome_list), 2)
             self.chromosome_bin = bin(self.chromosome_dec)[2:].zfill(self.chromosome_length)
-        self.keff = 0
+        self.keff = 0.001
 
     def get_uranium_radius(self):
         value =  int(self.chromosome_bin[0:16],base=2)
-        return 1 + (value * 14.0) / float((2 ** 16) - 1)
+        return 1 + (value * 10.0) / float((2 ** 16) - 1)
 
     def get_water_radius(self):
         value = int(self.chromosome_bin[16:32],base=2)
-        return 15 + (value * 35) / float((2 ** 16) - 1)
+        return 9 + (value * 31) / float((2 ** 16) - 1)
 
     def get_fitness(self):
-        dens_uranium = 10.1
+        dens_uranium = 19.1
         dens_water = 1.0
-        vol_uranium = 4.2*(self.get_uranium_radius())**3
-        vol_water = 4.2*(self.get_water_radius()-self.get_uranium_radius())**3
-        return 1 / (abs(1 - self.keff) * math.sqrt(dens_uranium * vol_uranium + dens_water * vol_water))
+        vol_uranium = 4.2 * (self.get_uranium_radius()) ** 3
+        vol_water = 4.2 * (self.get_water_radius() - self.get_uranium_radius()) ** 3
+        return (100 * math.exp(-20.0 * abs(self.keff - 1))) / (dens_uranium * vol_uranium + dens_water * vol_water)
 
     def __str__(self):
         return "Chromosome with {} fitness, uranium radius: {}, water radius: {}, keff: {}, list: {}, dec: {}, bin: {}".format(self.get_fitness(), self.get_uranium_radius(), self.get_water_radius(),
