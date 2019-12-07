0acde89cf4e81b625736ed4bbd5fd451284526e4
Piotr Sawicki <37271385+psrubing@users.noreply.github.com>
2019-12-07 18:53:49 +0100

    Update Population.py

--- Population.py
+++ Population.py
@@ -2,6 +2,7 @@ import random
 import os
 from Chromosome import Chromosome
 
+
 class Population:
 
     def __init__(self, pop_size):
@@ -10,7 +11,6 @@ class Population:
         self.generation = 0
         self.pop_fitness = 0
         self.dir_path = "/home/piotrs/GA/Core+Boron/Generation_"
-
 
     def make_population(self):
 
@@ -60,16 +60,26 @@ class Population:
             new_chromosomes.append(Chromosome(new_chromosome))
         self.chromosomes = new_chromosomes
 
-    def best_chromosome(self,fit_or_chrom = 0):
-        best = 0.0
+    def best_chromosome(self, fit_or_chrom=0):
+        best_fitness = 0.0
+        best_dec = 0
+        best_keff_nominal = 0.0
+        best_keff_void = 0.0
         for chromosome in self.chromosomes:
-            if best < chromosome.get_fitness():
-                best = chromosome.get_fitness()
-                best_chromosome = chromosome.chromosome_dec
+            if best_fitness < chromosome.get_fitness():
+                best_fitness = chromosome.get_fitness()
+                best_dec = chromosome.chromosome_dec
+                best_keff_nominal = chromosome.keff_nominal
+                best_keff_void = chromosome.keff_void
+
         if fit_or_chrom == 0:
-            return best_chromosome
+            return best_fitness
         elif fit_or_chrom == 1:
-            return best
+            return best_dec
+        elif fit_or_chrom == 2:
+            return best_keff_nominal
+        elif fit_or_chrom == 3:
+            return best_keff_void
 
     def makedir(self):
         os.mkdir(self.dir_path + str(self.generation))
@@ -78,7 +88,8 @@ class Population:
         counter = 1
         for chromosome in self.chromosomes:
             pattern_nominal = open("/home/piotrs/GA/Core+Boron/input_nominal.inp", "r")
-            chromosome_nominal_path = self.dir_path +str(self.generation)+"/" + str(counter) + "_" + str(chromosome.chromosome_dec) + "_nominal" + ".inp"
+            chromosome_nominal_path = self.dir_path + str(self.generation) + "/" + str(counter) + "_" + str(
+                chromosome.chromosome_dec) + "_nominal" + ".inp"
             chromosome_input_nominal = open(chromosome_nominal_path, "w")
             for line in pattern_nominal:
                 if "surf 11 pz" in line:
@@ -94,13 +105,15 @@ class Population:
             pattern_nominal.close()
             chromosome_input_nominal.close()
 
-            bash_path_nominal = self.dir_path + str(self.generation) + "/" + str(counter) + "_" + str(chromosome.chromosome_dec) + "_nominal" + ".sh"
+            bash_path_nominal = self.dir_path + str(self.generation) + "/" + str(counter) + "_" + str(
+                chromosome.chromosome_dec) + "_nominal" + ".sh"
             bash_nominal_input = open(bash_path_nominal, "w")
             bash_cmd = "#!/bin/bash\n/home/piotrs/GA/Core+Boron/sss2 -omp 4 "
-            bash_nominal_input.write(bash_cmd + self.dir_path + str(self.generation) + "/" + str(counter) + "_" + str(chromosome.chromosome_dec) + "_nominal" + ".inp")
+            bash_nominal_input.write(bash_cmd + self.dir_path + str(self.generation) + "/" + str(counter) + "_" + str(
+                chromosome.chromosome_dec) + "_nominal" + ".inp")
             bash_nominal_input.close()
 
-            #print("\nSymulacja dla chromosomu: {} z pokolenia {}".format(chromosome, self.generation))
+            # print("\nSymulacja dla chromosomu: {} z pokolenia {}".format(chromosome, self.generation))
             os.system("bash " + bash_path_nominal)
 
             chromosome_nominal_output = open(chromosome_nominal_path + "_res.m", "r")
@@ -108,8 +121,9 @@ class Population:
                 if "ABS_KEFF" in line:
                     chromosome.keff_nominal = float(line[47:58])
 
-            pattern_voided = open("/home/piotrs/GA/Core+Boron/input_voided.inp", "r")
-            chromosome_voided_path = self.dir_path + str(self.generation) + "/" + str(counter) + "_" + str(chromosome.chromosome_dec) + "_voided" + ".inp"
+            pattern_voided = open("/home/piotrs/GA/Core+Boron/input_void.inp", "r")
+            chromosome_voided_path = self.dir_path + str(self.generation) + "/" + str(counter) + "_" + str(
+                chromosome.chromosome_dec) + "_void" + ".inp"
             chromosome_input_voided = open(chromosome_voided_path, "w")
             for line in pattern_voided:
                 if "surf 11 pz" in line:
@@ -125,18 +139,20 @@ class Population:
             pattern_voided.close()
             chromosome_input_voided.close()
 
-            bash_path_voided = self.dir_path + str(self.generation) + "/" + str(counter) + "_" + str(chromosome.chromosome_dec) + "_voided" + ".sh"
+            bash_path_voided = self.dir_path + str(self.generation) + "/" + str(counter) + "_" + str(
+                chromosome.chromosome_dec) + "_void" + ".sh"
             bash_voided_input = open(bash_path_voided, "w")
             bash_cmd = "#!/bin/bash\n/home/piotrs/GA/Core+Boron/sss2 -omp 4 "
-            bash_voided_input.write(bash_cmd + self.dir_path + str(self.generation) + "/" + str(counter) + "_" + str(chromosome.chromosome_dec) + "_voided" + ".inp")
+            bash_voided_input.write(bash_cmd + self.dir_path + str(self.generation) + "/" + str(counter) + "_" + str(
+                chromosome.chromosome_dec) + "_void" + ".inp")
             bash_voided_input.close()
 
-            #print("\nSymulacja dla chromosomu: {} z pokolenia {}".format(chromosome, self.generation))
+            # print("\nSymulacja dla chromosomu: {} z pokolenia {}".format(chromosome, self.generation))
             os.system("bash " + bash_path_voided)
 
             chromosome_voided_output = open(chromosome_voided_path + "_res.m", "r")
             for line in chromosome_voided_output:
                 if "ABS_KEFF" in line:
-                    chromosome.keff_voided = float(line[47:58])
+                    chromosome.keff_void = float(line[47:58])
 
             counter += 1
