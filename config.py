ef185e52ddaa5b327476eb2fab54be104e641c48
Piotr Sawicki <37271385+psrubing@users.noreply.github.com>
2020-01-27 16:21:29 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+# Simulation settings
+num_generations = 50
+pop_size = 10
+mutation_prob = 0.05
+
+# directories and files settings
+best_path = "/home/piotrs/GA/Core+Boron/Best_Chromosomes.txt"                                               # best from gen file
+dir_path = "/home/piotrs/GA/Core+Boron/Generation_"                                                         # results folders
+serpent_path = "/home/piotrs/GA/Core+Boron/sss2"                                                            # serpent file
+patterns = ["/home/piotrs/GA/Core+Boron/input_nominal.inp", "/home/piotrs/GA/Core+Boron/input_void.inp"]    #serpent inputs
