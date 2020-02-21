07595a8664492be1a747ca7c97e9be52be85461c
Piotr Sawicki <37271385+psrubing@users.noreply.github.com>
2020-02-21 16:41:29 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 # Simulation settings
-num_generations = 50
-pop_size = 10
-mutation_prob = 0.05
+num_generations = 50    # number of iterated generations
+pop_size = 10           # amount of individuals in population
+mutation_prob = 0.05    # mutaion rate (double between 0.0 and 1.0)
 
 # directories and files settings
-best_path = "/home/piotrs/GA/Core+Boron/Best_Chromosomes.txt"                                               # best from gen file
-dir_path = "/home/piotrs/GA/Core+Boron/Generation_"                                                         # results folders
-serpent_path = "/home/piotrs/GA/Core+Boron/sss2"                                                            # serpent file
-patterns = ["/home/piotrs/GA/Core+Boron/input_nominal.inp", "/home/piotrs/GA/Core+Boron/input_void.inp"]    #serpent inputs
+project_dir = "~/"                                                                             # main project directory path
+best_path = project_dir + "Best_Chromosomes.txt"                                               # best from gen file
+dir_path = project_dir + "Generation_"                                                         # results folders
+serpent_path = project_dir + "sss2"                                                            # serpent file
+patterns = [project_dir + "input_nominal.inp", project_dir + "input_void.inp"]                 # serpent inputs
